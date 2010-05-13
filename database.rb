5946e9bb5399ec614f3dd46257d6fbc720bd0847
Sean Kermes <seank@softartisans.com>
2010-05-13 12:15:01 -0400

    Inserting the names of songs along with their codes.

--- database.rb
+++ database.rb
@@ -8,14 +8,13 @@ def random_tapes(n)
 	database.execute("select name from tape order by random() limit #{n};").collect { |row| row[0] }
 end
 
-def create_new_tape(name, description, song_codes)
+def create_new_tape(name, description, songs)
 	db = database
 	db.execute("insert into tape (name, description) values ('#{name}', '#{description}');")
-	tape_id = db.execute("select id from tape where name = '#{name}';")[0][0]
-	puts song_codes
-	song_codes.each_index do |n|
-		db.execute("insert into song (yt_code) values ('#{song_codes[n]}');")
-		song_id = db.execute("select id from song where yt_code = '#{song_codes[n]}';")[0][0]
+	tape_id = db.execute("select id from tape where name = '#{name}';")[0][0]	
+	songs.each_index do |n|
+		db.execute("insert into song (yt_code, name) values ('#{songs[n][0]}', '#{songs[n][1].delete '\''}');")
+		song_id = db.execute("select id from song where yt_code = '#{songs[n][0]}';")[0][0]
 		db.execute("insert into play (tapeid, songid, [order]) values (#{tape_id}, #{song_id}, #{n});")
 	end
 end
--- twelve.rb
+++ twelve.rb
@@ -1,10 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'net/http'
 
 require 'database'
 
 $yt_url_regex = /(http:\/\/)?(www\.)?youtube\.com\/watch\?v=(.{11})/
+$yt_title_regex = /<meta name="title" content="([^>]*)">/
 
 def render_master(head, body)
 	@head = head
@@ -23,11 +25,14 @@ end
 
 post '/create' do
 	name = params[:name].delete " \t\r\n"
-	song_codes = (1..12).collect do |n|
+	songs = (1..12).collect do |n|
 		params["song_#{n}"] =~ $yt_url_regex
-		$3
+		code = $3
+		watch_page = Net::HTTP.get URI.parse("http://www.youtube.com/watch?v=#{code}")
+		watch_page =~ $yt_title_regex
+		[code, $1]
 	end
-	create_new_tape(name, params[:description], song_codes)
+	create_new_tape(name, params[:description], songs)
 	
 	redirect "/#{name}", 303 # http://www.gittr.com/index.php/archive/details-of-sinatras-redirect-helper/
 end
