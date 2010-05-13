a54837f699e69e3d7f0d1450b05f61cfd675faae
Sean Kermes <seank@softartisans.com>
2010-05-13 11:12:38 -0400

    Early successes!  We embed videos and have play/pause links.

--- public/tape.js
+++ public/tape.js
@@ -1,6 +1,60 @@
+player = { yt: null,
+		   is_playing: false,
+		   current: null };
+
+function ytPlayerStateChanged(state) {
+	if (player.yt) {
+		if (state == -1) { // unstarted
+		}
+		else if (state == 0) { // ended
+		}
+		else if (state == 1) { // playing
+		}
+		else if (state == 2) { // paused
+		}
+		else if (state == 3) { // buffering
+		}
+		else if (state == 5) { // cued, ready to play
+			player.yt.playVideo();
+		}
+	}
+}
+		   
+function linkClicked(e) {
+	e.preventDefault();
+
+	var href = e.target.href;
+		
+	if (player.yt) {
+		if (!player.current || player.current != href) {			
+			player.current = href;
+			player.is_playing = true;
+			player.yt.loadVideoByUrl(href);
+		}
+		else if (player.is_playing) {			
+			player.yt.pauseVideo();
+			player.is_playing = false;
+		}
+		else {
+			player.yt.playVideo();
+			player.is_playing = true;
+		}
+	}	
+}
+
+function onYouTubePlayerReady(playerid) {
+	player.yt = document.getElementById("myytplayer");
+	player.yt.addEventListener("onStateChange", "ytPlayerStateChanged");
+	$(".play_link").click(linkClicked);
+}
 
 function init() {
-	alert("init");
+	var params = { allowScriptAccess: "always" };
+	var attrs = { id: "myytplayer" };
+	
+	var firsturl = $(".play_link").attr("href");
+	
+	swfobject.embedSWF(firsturl, "ytapiplayer", "425", "356", "8", null, null, params, attrs);
 }
 
 $(document).ready(init);
