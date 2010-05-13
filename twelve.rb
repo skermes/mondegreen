25b5c88194ecfac4ec3b425b4b7ad2ef1ebc8fc0
Sean Kermes <seank@softartisans.com>
2010-05-13 08:54:03 -0400

    Adding simple intro view.

--- twelve.rb
+++ twelve.rb
@@ -2,6 +2,13 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
+def render_master(head, body)
+	@head = head
+	@body = body
+	haml :master
+end
+
 get '/' do
-	"twelve songs"
+	@tapes = ['one', 'two']
+	render_master('', :splash_body)
 end
