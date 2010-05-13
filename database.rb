a887018b29bf26291552ded6e072f7491ade376b
Sean Kermes <seank@softartisans.com>
2010-05-13 10:00:40 -0400

    Creating a view that lets you create new tapes.

--- database.rb
+++ database.rb
@@ -5,5 +5,17 @@ def database()
 end
 
 def random_tapes(n)
-	database.execute("select name from tape order by random() limit #{n}").collect { |row| row[0] }
+	database.execute("select name from tape order by random() limit #{n};").collect { |row| row[0] }
+end
+
+def create_new_tape(name, description, song_codes)
+	db = database
+	db.execute("insert into tape (name, description) values ('#{name}', '#{description}');")
+	tape_id = db.execute("select id from tape where name = '#{name}';")[0][0]
+	puts song_codes
+	song_codes.each_index do |n|
+		db.execute("insert into song (yt_code) values ('#{song_codes[n]}');")
+		song_id = db.execute("select id from song where yt_code = '#{song_codes[n]}';")[0][0]
+		db.execute("insert into play (tapeid, songid, [order]) values (#{tape_id}, #{song_id}, #{n});")
+	end
 end
--- twelve.rb
+++ twelve.rb
@@ -4,6 +4,8 @@ require 'haml'
 
 require 'database'
 
+$yt_url_regex = /(http:\/\/)?(www\.)?youtube\.com\/watch\?v=(.{11})/
+
 def render_master(head, body)
 	@head = head
 	@body = body
@@ -17,4 +19,19 @@ end
 
 get '/create' do
 	render_master '', :create_body
+end
+
+post '/create' do
+	name = params[:name].delete " \t\r\n"
+	song_codes = (1..12).collect do |n|
+		params["song_#{n}"] =~ $yt_url_regex
+		$3
+	end
+	create_new_tape(name, params[:description], song_codes)
+	
+	redirect "/#{name}", 303 # http://www.gittr.com/index.php/archive/details-of-sinatras-redirect-helper/
+end
+
+get '/:name' do
+	"you have a tape named #{params[:name]}"
 end
