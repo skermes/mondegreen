f9b98307bbc06217503592f6c62945e986b7b37f
Sean Kermes <seank@softartisans.com>
2010-07-22 18:12:56 -0400

    Adding some basic searching and adding to a list functionality.

---
+++ public/create.js
@@ -0,0 +1,36 @@
+function addsong(event) {	
+	var inputs = $('.songinput');
+	inputs.filter(function (index) { return inputs[index].value == ''; }).first().val(event.target.id);
+}
+
+function vidUrl(id) {	
+	 return 'http://www.youtube.com/watch?v=' + id.substring(id.length - 11, id.length);
+}
+
+function displaysearch(data, status) {
+	if (status != 'success') {	
+		return;
+	}	
+	
+	var list = $('#results');
+	list.children().remove();
+	for (var i = 0; i < data.feed.entry.length; i++) {
+		list.append('<li class="resultlink" id="' + vidUrl(data.feed.entry[i].id.$t) + '">' + data.feed.entry[i].title.$t + '</li>');
+	}
+	
+	$('.resultlink').click(addsong);
+}
+
+function dosearch() {	
+	var url = 'http://gdata.youtube.com/feeds/api/videos';
+	url += '?q=' + $('#query').val();
+	url += '&orderby=relevance';
+	url += '&alt=json';
+	
+	$.getJSON(url, displaysearch); 
+	return false;
+}
+
+$(document).ready(function() {
+	$('#search').submit(dosearch);
+});
